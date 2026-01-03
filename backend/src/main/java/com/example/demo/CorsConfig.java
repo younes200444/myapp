574810506ef88897b7a2d1bb27b3b0ea6da16578
younes200444 <youneselamrani015@gmail.com>
2026-01-03 23:56:46 +0100

    Update CorsConfig.java

--- backend/src/main/java/com/example/demo/CorsConfig.java
+++ backend/src/main/java/com/example/demo/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
